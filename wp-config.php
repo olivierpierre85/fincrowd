7e9505ffa6854d9dfcfce2152617ebe3f41e8673
olivierpierre85 <olivierpierre85@gmail.com>
2017-06-27 12:40:47 +0200

    wrong db name

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'fincrowd2');
+define('DB_NAME', 'fincrowd');
 
 /** MySQL database username */
 define('DB_USER', 'root');
