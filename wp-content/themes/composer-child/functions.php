64c4196804b255c21ce03632262dcbb0835f27a0
olivierpierre85 <olivierpierre85@gmail.com>
2018-02-13 15:50:50 +0100

    Hide items in menu when not logged in, and hide add campaign when physical person

--- wp-content/themes/composer-child/functions.php
+++ wp-content/themes/composer-child/functions.php
@@ -1,5 +1,7 @@
 <?php
+//MENU ITEMS HANDLING
 
+//LOGIN LOGOUT ITEMS
 add_filter('wp_nav_menu_items', 'add_login_logout_link', 10, 2);
 function add_login_logout_link($items, $args) {
         ob_start();
@@ -9,6 +11,37 @@ function add_login_logout_link($items, $args) {
         $items .= '<li>'. $loginoutlink .'</li>';
     return $items;
 }
+
+add_filter( 'wp_get_nav_menu_items', 'wpneo_fi_exclude_menu_items', null, 3 );
+function wpneo_fi_exclude_menu_items( $items, $menu, $args ) {
+    // Iterate over the items to search and destroy
+    foreach ( $items as $key => $item ) {
+      if(is_user_logged_in()) {
+        if ( $item->ID == 16 ) unset( $items[$key] );
+        //and if not moral person, hide create campaign
+        if(get_the_author_meta( 'physical_person', get_current_user_id() )){
+          if ( $item->ID == 15 ) unset( $items[$key] );
+        }
+      } else {
+        if ( $item->ID == 15 ) unset( $items[$key] );
+        if ( $item->ID == 17 ) unset( $items[$key] );
+      }
+    }
+
+    return $items;
+}
+
+//HIDE is not connected
+// add_filter( 'wp_setup_nav_menu', function( \stdClass $item ) {
+//     # Check conditionals, and invalidate an item in case
+//     $item->_invalid = is_user_logged_in()
+//         && 'post' === $item->object
+//         && 'post_type' === $item->type
+//         # && … whatever you need to check for your invalidation of an item
+//     ;
+//
+//     return $item;
+// } );
 
 
 /* *********************************************************************************************
